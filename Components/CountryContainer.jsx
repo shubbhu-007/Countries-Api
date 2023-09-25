2175462b777833ab4f18e553320663e270f93b5e
Shubham Sonkar <shubhamsonkar5657@gmail.com>
2023-09-25 13:59:45 -0500

    ui rendered with fetched data

--- Components/CountryContainer.jsx
+++ Components/CountryContainer.jsx
@@ -1,12 +1,25 @@
+import {useEffect , useState} from "react";
 import Data from "../Data.json";
 import CountryCard from "../Components/CountryCard"
 // console.log(Data);
 const CountryContainer = () => {
-    return (
+    const [data,setData] = useState([]);
+
+    async function getData(){
+        const countrylist = await fetch("https://restcountries.com/v3.1/all");
+        const res = await countrylist.json();
+        setData(res);
+    }
+
+    useEffect(() => {
+        getData();
+    },[])
+
+    return (data.length === 0)? "Loading ....." :(
         <div className="countries-container">
             
             {
-                Data.map((country ,index) => {
+                data.map((country ,index) => {
                    return <CountryCard {...country}  key={index} />
                 })
             }
